99d87464db26fec1eefc0706d6822f12730f86ed
William Reynolds <williamareynolds@icloud.com>
2020-06-06 16:03:34 -0500

    chore: test the fast-check integration

--- src/index.spec.ts
+++ src/index.spec.ts
@@ -1,7 +1,18 @@
+import * as fc from 'fast-check'
 import * as i from './index'
 
 describe('add', () => {
   it('is plain addition', () => {
     expect(i.add (1) (2)).toBe(3)
+  })
+
+  it('obeys the commutative property', () => {
+    fc.assert(fc.property(
+      fc.oneof(fc.integer(), fc.float()),
+      fc.oneof(fc.integer(), fc.float()),
+      (a, b) => {
+        return i.add (a) (b) === i.add (b) (a)
+      }
+    ))
   })
 })
