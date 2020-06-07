ecd300bb709f6befdfa714a0a449789ddbf45d22
William Reynolds <williamareynolds@icloud.com>
2020-06-07 17:32:08 -0500

    feat(check-endpoint): add the capability to assert a status code
    checkEndpointStatus represents a structure that will be common accross a lot of the assertion
    mechanism. Via monadic binding, each check can be executed creating an opportunity to fail. It may
    be wise to move to a Witherable instance of the current structure so errors from different
    assertions can be accumulated. At this time, the checkEndpoint function performs actual useful work.
    The next step is to run assertions from schemata.

--- src/lib/check-endpoint.spec.ts
+++ src/lib/check-endpoint.spec.ts
@@ -1,10 +1,56 @@
+import * as fc from 'fast-check'
 import * as E from 'fp-ts/lib/Either'
-import { checkEndpoint } from './check-endpoint'
+import { checkEndpoint, checkEndpointStatus } from './check-endpoint'
+import axios from 'axios'
+
 
 describe('check-endpoint', () => {
+  describe('checkEndpointStatus', () => {
+    it('returns success when args match', () => {
+      fc.assert(fc.property(
+        fc.integer(),
+        (num) => {
+          const expected = {
+            status: num
+          }
+          const actual = checkEndpointStatus(num)(expected)
+          expect(E.isRight(actual)).toBe(true)
+          E.either.map(actual, r => {
+            expect(r).toStrictEqual(expected)
+          })
+        }
+      ))
+    })
+
+    it('returns failure when args differ', () => {
+      fc.assert(fc.property(
+        fc.integer(100, 299),
+        fc.record({ status: fc.integer(300, 599) }),
+        (a, b) => {
+          const actual = checkEndpointStatus(a)(b)
+          expect(E.isLeft(actual)).toBe(true)
+          E.either.mapLeft(actual, err => {
+            expect(err.message).toContain(a.toString(10))
+            expect(err.message).toContain(b.status.toString(10))
+          })
+        }
+      ))
+    })
+  })
+
   describe('checkEndpoint', () => {
-    it('returns a success state', () => {
-      expect(E.isRight(checkEndpoint())).toBe(true)
+    it('returns a success state', async () => {
+      axios.get = jest.fn(async () => ({ status: 200 }) as any)
+
+      const url = 'http://abc.com/api/health'
+      const expectations = {
+        status: 200
+      }
+      const actual = await checkEndpoint({ url, expectations })()
+      expect(E.isRight(actual))
+
+      expect(axios.get).toHaveBeenCalledTimes(1)
+      expect(axios.get).toHaveBeenCalledWith(url)
     })
   })
 })
--- src/lib/check-endpoint.ts
+++ src/lib/check-endpoint.ts
@@ -1,18 +1,53 @@
 import * as E from 'fp-ts/lib/Either'
+import * as T from 'fp-ts/lib/Task'
+import * as RTE from 'fp-ts/lib/ReaderTaskEither'
+import axios, { AxiosResponse } from 'axios'
+import { pipe } from 'fp-ts/lib/pipeable'
 
-const SUCCESS = 'success'
-type Success = typeof SUCCESS
+type Response = AxiosResponse<unknown>
+type Success = Response
 
 type Failure = {
   message: string
 }
 
-const checkEndpoint = (): E.Either<Failure, Success> => {
-  return E.right(SUCCESS)
+type Expectations = {
+  status: number
+}
+
+type CheckEndpointDeps = {
+  url: string,
+  expectations: Expectations
+}
+
+const getTask = (url: string): T.Task<AxiosResponse<unknown>> => async () => {
+  return axios.get(url)
+}
+
+interface HasStatus {
+  status: number
+}
+
+const checkEndpointStatus = (expected: number) => <R extends HasStatus>(actual: R): E.Either<Failure, R> => {
+  return expected === actual.status
+    ? E.right(actual)
+    : E.left({
+      message: `Expected status ${expected} but received status ${actual.status}`
+    })
+}
+
+const checkEndpoint: RTE.ReaderTaskEither<CheckEndpointDeps, Failure, Success> = (deps) => {
+  const res = pipe(
+    getTask(deps.url),
+    T.map(checkEndpointStatus(deps.expectations.status))
+  )
+
+  return res
 }
 
 export {
   checkEndpoint,
+  checkEndpointStatus,
   Failure,
   Success
 }
