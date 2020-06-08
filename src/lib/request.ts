da84c5084926504a9c39ce198990e1c57a53fea3
William Reynolds <williamareynolds@icloud.com>
2020-06-08 00:46:32 -0500

    style(request): add type and clean up getTask

--- src/lib/request.ts
+++ src/lib/request.ts
@@ -1,6 +1,8 @@
 import * as T from 'fp-ts/lib/Task'
 import axios, { AxiosResponse } from 'axios'
 
-export const getTask = (url: string): T.Task<AxiosResponse<unknown>> => async () => {
-  return axios.get(url)
-}
+export const getTask =
+  (url: string): T.Task<AxiosResponse<unknown>> =>
+    async (): Promise<AxiosResponse<unknown>> => {
+      return axios.get(url)
+    }
